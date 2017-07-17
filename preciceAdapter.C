12f48b3f48a6befed3ecef00893fe6e17d7e2282
MakisH <makishourdakis@gmail.com>
2017-07-17 19:37:34 +0200

    Modify the header for License/Copyright.

--- preciceAdapter.C
+++ preciceAdapter.C
@@ -1,25 +1,22 @@
 /*---------------------------------------------------------------------------*\
-  =========                 |
-  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2016 OpenFOAM Foundation
-     \\/     M anipulation  |
--------------------------------------------------------------------------------
-License
-    This file is part of OpenFOAM.
+preCICE-adapter for OpenFOAM
 
-    OpenFOAM is free software: you can redistribute it and/or modify it
+Copyright (c) 2017 Gerasimos Chourdakis
+-------------------------------------------------------------------------------
+
+License
+    This adapter is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    This adapter is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
     for more details.
 
     You should have received a copy of the GNU General Public License
-    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+    along with the adapter.  If not, see <http://www.gnu.org/licenses/>.
 
 \*---------------------------------------------------------------------------*/
 
