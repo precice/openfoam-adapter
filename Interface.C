21c00e8bd6b6f750fb41918079bd91a40f70f688
MakisH <makishourdakis@gmail.com>
2017-09-07 19:49:08 +0200

    Cleanup the Interface class.

--- Interface.C
+++ Interface.C
@@ -2,142 +2,191 @@
 
 using namespace Foam;
 
-preciceAdapter::Interface::Interface( precice::SolverInterface & precice, const fvMesh& mesh, std::string meshName, std::vector<std::string> patchNames ) :
-	_precice( precice ),
-	_meshName( meshName ),
-	_patchNames( patchNames ),
-	_numDataLocations( 0 ),
-	_numDims( 3 )
+preciceAdapter::Interface::Interface
+(
+    precice::SolverInterface & precice,
+    const fvMesh& mesh,
+    std::string meshName,
+    std::vector<std::string> patchNames
+)
+:
+precice_(precice),
+meshName_(meshName),
+patchNames_(patchNames)
 {
-	_meshID = _precice.getMeshID( _meshName );
+    meshID_ = precice_.getMeshID(meshName_);
 
-	for( uint i = 0 ; i < patchNames.size() ; i++ )
-	{
-		int patchID = mesh.boundaryMesh().findPatchID( patchNames.at( i ) );
-		if( patchID == -1 )
-		{
-			// BOOST_LOG_TRIVIAL( error ) << "ERROR: Patch '" << patchNames.at( i ) << "' does not exist.";
-			exit( 1 );
-		}
+    for (uint i = 0; i < patchNames.size(); i++)
+    {
+        int patchID = mesh.boundaryMesh().findPatchID(patchNames.at(i));
 
-		_patchIDs.push_back( patchID );
-	}
-	_configureMesh( mesh );
+        if (patchID == -1)
+        {
+            FatalErrorInFunction
+                 << "ERROR: Patch '"
+                 << patchNames.at(i)
+                 << "' does not exist."
+                 << exit(FatalError);
+        }
 
-	/* An interface has only one data buffer, which is shared between several CouplingDataReaders and CouplingDataWriters
-	   The initial allocation assumes scalar data, if CouplingDataReaders or -Writers have vector data, it is resized (TODO) */
-	_dataBuffer = new double[_numDataLocations]();
+        patchIDs_.push_back(patchID);
+    }
+
+    configureMesh_(mesh);
+
+    //- An interface has only one data buffer, which is shared between several
+    //  CouplingDataReaders and CouplingDataWriters.
+    //  The initial allocation assumes scalar data.
+    //  If CouplingDataReaders or -Writers have vector data, it is resized (TODO)
+    dataBuffer_ = new double[numDataLocations_]();
 }
 
-void preciceAdapter::Interface::_configureMesh( const fvMesh& mesh )
+void preciceAdapter::Interface::configureMesh_(const fvMesh& mesh)
 {
-	for( uint k = 0 ; k < _patchIDs.size() ; k++ )
-	{
-		_numDataLocations += mesh.boundaryMesh()[_patchIDs.at( k )].faceCentres().size();
-	}
-	int vertexIndex = 0;
-	double vertices[3 * _numDataLocations];
-	_vertexIDs = new int[_numDataLocations];
+    for (uint k = 0; k < patchIDs_.size(); k++)
+    {
+        numDataLocations_ +=
+            mesh.boundaryMesh()[patchIDs_.at(k)].faceCentres().size();
+    }
 
-	for( uint k = 0 ; k < _patchIDs.size() ; k++ )
-	{
-		const vectorField & faceCenters = mesh.boundaryMesh()[_patchIDs.at( k )].faceCentres();
+    int vertexIndex = 0;
+    double vertices[3 * numDataLocations_];
+    vertexIDs_ = new int[numDataLocations_];
 
-		for( int i = 0 ; i < faceCenters.size() ; i++ )
-		{
-			vertices[vertexIndex++] = faceCenters[i].x();
-			vertices[vertexIndex++] = faceCenters[i].y();
-			vertices[vertexIndex++] = faceCenters[i].z();
-		}
-	}
-	_precice.setMeshVertices( _meshID, _numDataLocations, vertices, _vertexIDs );
+    for (uint k = 0; k < patchIDs_.size(); k++)
+    {
+        const vectorField & faceCenters =
+            mesh.boundaryMesh()[patchIDs_.at(k)].faceCentres();
 
+        for (int i = 0; i < faceCenters.size(); i++)
+        {
+            vertices[vertexIndex++] = faceCenters[i].x();
+            vertices[vertexIndex++] = faceCenters[i].y();
+            vertices[vertexIndex++] = faceCenters[i].z();
+        }
+    }
+
+    precice_.setMeshVertices(meshID_, numDataLocations_, vertices, vertexIDs_);
 }
 
 
-void preciceAdapter::Interface::addCouplingDataWriter( std::string dataName, CouplingDataWriter * couplingDataWriter )
+void preciceAdapter::Interface::addCouplingDataWriter
+(
+    std::string dataName,
+    CouplingDataWriter * couplingDataWriter
+)
 {
-	couplingDataWriter->setDataID( _precice.getDataID( dataName, _meshID ) );
-	couplingDataWriter->setPatchIDs( _patchIDs );
-	_couplingDataWriters.push_back( couplingDataWriter );
+    couplingDataWriter->setDataID(precice_.getDataID(dataName, meshID_));
+    couplingDataWriter->setPatchIDs(patchIDs_);
+    couplingDataWriters_.push_back(couplingDataWriter);
 
-	if( couplingDataWriter->hasVectorData() )
-	{
-		// TODO: Resize buffer for vector data (if not already resized)
-	}
+    if (couplingDataWriter->hasVectorData())
+    {
+        // TODO: Resize buffer for vector data (if not already resized)
+    }
 }
 
 
-void preciceAdapter::Interface::addCouplingDataReader( std::string dataName, preciceAdapter::CouplingDataReader * couplingDataReader )
+void preciceAdapter::Interface::addCouplingDataReader
+(
+    std::string dataName,
+    preciceAdapter::CouplingDataReader * couplingDataReader
+)
 {
-	couplingDataReader->setDataID( _precice.getDataID( dataName, _meshID ) );
-	couplingDataReader->setPatchIDs( _patchIDs );
-	_couplingDataReaders.push_back( couplingDataReader );
+    couplingDataReader->setDataID(precice_.getDataID(dataName, meshID_));
+    couplingDataReader->setPatchIDs(patchIDs_);
+    couplingDataReaders_.push_back(couplingDataReader);
 
-	if( couplingDataReader->hasVectorData() )
-	{
-		// TODO: Resize buffer for vector data (if not already resized)
-	}
+    if (couplingDataReader->hasVectorData())
+    {
+        // TODO: Resize buffer for vector data (if not already resized)
+    }
 }
 
 
 void preciceAdapter::Interface::readCouplingData()
 {
-	if( _precice.isReadDataAvailable() )
-	{
-		for( uint i = 0 ; i < _couplingDataReaders.size() ; i++ )
-		{
-			preciceAdapter::CouplingDataReader * couplingDataReader = _couplingDataReaders.at( i );
+    if (precice_.isReadDataAvailable())
+    {
+        for (uint i = 0; i < couplingDataReaders_.size(); i++)
+        {
+            preciceAdapter::CouplingDataReader *
+                couplingDataReader = couplingDataReaders_.at(i);
 
-			if( couplingDataReader->hasVectorData() )
-			{
-				_precice.readBlockVectorData( couplingDataReader->dataID(), _numDataLocations, _vertexIDs, _dataBuffer );
-			}
-			else
-			{
-				_precice.readBlockScalarData( couplingDataReader->dataID(), _numDataLocations, _vertexIDs, _dataBuffer );
-			}
-			couplingDataReader->read( _dataBuffer );
-		}
-	}
+            if (couplingDataReader->hasVectorData())
+            {
+                precice_.readBlockVectorData
+                (
+                    couplingDataReader->dataID(),
+                    numDataLocations_,
+                    vertexIDs_,
+                    dataBuffer_
+                );
+            }
+            else
+            {
+                precice_.readBlockScalarData
+                (
+                    couplingDataReader->dataID(),
+                    numDataLocations_,
+                    vertexIDs_,
+                    dataBuffer_
+                );
+            }
+
+            couplingDataReader->read(dataBuffer_);
+        }
+    } // TODO: Else what?
 }
 
 void preciceAdapter::Interface::writeCouplingData()
 {
-	for( uint i = 0 ; i < _couplingDataWriters.size() ; i++ )
-	{
-		preciceAdapter::CouplingDataWriter * couplingDataWriter = _couplingDataWriters.at( i );
-		couplingDataWriter->write( _dataBuffer );
+    for (uint i = 0; i < couplingDataWriters_.size(); i++)
+    {
+        preciceAdapter::CouplingDataWriter *
+            couplingDataWriter = couplingDataWriters_.at(i);
 
-		if( couplingDataWriter->hasVectorData() )
-		{
-			_precice.writeBlockVectorData( couplingDataWriter->dataID(), _numDataLocations, _vertexIDs, _dataBuffer );
-		}
-		else
-		{
-			_precice.writeBlockScalarData( couplingDataWriter->dataID(), _numDataLocations, _vertexIDs, _dataBuffer );
-		}
-	}
+        couplingDataWriter->write(dataBuffer_);
+
+        if (couplingDataWriter->hasVectorData())
+        {
+            precice_.writeBlockVectorData
+            (
+                couplingDataWriter->dataID(),
+                numDataLocations_,
+                vertexIDs_,
+                dataBuffer_
+            );
+        }
+        else
+        {
+            precice_.writeBlockScalarData
+            (
+                couplingDataWriter->dataID(),
+                numDataLocations_,
+                vertexIDs_,
+                dataBuffer_
+            );
+        }
+    }
 }
 
 preciceAdapter::Interface::~Interface()
 {
+    for (uint i = 0; i < couplingDataReaders_.size(); i++)
+    {
+        delete couplingDataReaders_.at(i);
+    }
+    couplingDataReaders_.clear();
 
-	// BOOST_LOG_TRIVIAL( info ) << "Destroying interface...";
+    for (uint i = 0; i < couplingDataWriters_.size(); i++)
+    {
+        delete couplingDataWriters_.at(i);
+    }
+    couplingDataWriters_.clear();
 
-	for ( uint i = 0 ; i < _couplingDataReaders.size() ; i++ )
-	{
-		delete _couplingDataReaders.at( i );
-	}
-	_couplingDataReaders.clear();
+    delete [] vertexIDs_;
 
-	for ( uint i = 0 ; i < _couplingDataWriters.size() ; i++ )
-	{
-		delete _couplingDataWriters.at( i );
-	}
-	_couplingDataWriters.clear();
-
-	delete [] _vertexIDs;
-	delete [] _dataBuffer;
+    delete [] dataBuffer_;
 
 }
