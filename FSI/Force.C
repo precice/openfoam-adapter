15139b2eb7dc568f89d52f0dda9e2036dbf3b0e0
Makis Chourdakis <makishourdakis@gmail.com>
2018-08-06 00:53:46 +0200

    Implement writing forces.

--- FSI/Force.C
+++ FSI/Force.C
@@ -11,6 +11,8 @@ preciceAdapter::FSI::Force::Force
     /  See CHT/Temperature.C for details.
     */
 )
+:
+mesh_(mesh)
 {
     dataType_ = vector;
 
@@ -35,6 +37,31 @@ preciceAdapter::FSI::Force::Force
     );
 }
 
+Foam::tmp<Foam::volSymmTensorField> preciceAdapter::FSI::Force::devRhoReff() const
+{
+    // TODO: Only works for laminar flows at the moment.
+    // See the OpenFOAM Forces function object, where an extended
+    // version of this method is being used.
+
+    // Get the kinematic viscosity from the transportProperties
+    // TODO: Get it from the objects' registry directly?
+    const dictionary& transportProperties =
+        mesh_.lookupObject<IOdictionary>("transportProperties");
+
+    dimensionedScalar nu(transportProperties.lookup("nu"));
+
+    // Get the velocity
+    const volVectorField& U = mesh_.lookupObject<volVectorField>("U");
+
+    // Get the density
+    // TODO Check
+    // TODO This probably only works with compressible solvers
+    const volScalarField& rho = mesh_.lookupObject<volScalarField>("rho");
+
+    return -rho * nu * dev(twoSymm(fvc::grad(U)));
+
+}
+
 void preciceAdapter::FSI::Force::write(double * buffer)
 {
     /* TODO: Implement
@@ -42,6 +69,63 @@ void preciceAdapter::FSI::Force::write(double * buffer)
     * the outer for the locations and the inner for the dimensions.
     * See the preCICE writeBlockVectorData() implementation.
     */
+    // Compute forces. See the Forces function object.
+    // Normal vectors on the boundary, multiplied with the face areas
+    const surfaceVectorField::Boundary& Sfb =
+        mesh_.Sf().boundaryField();
+
+    // Stress tensor
+    tmp<volSymmTensorField> tdevRhoReff = devRhoReff();
+
+    // Stress tensor boundary field
+    const volSymmTensorField::Boundary& devRhoReffb =
+        tdevRhoReff().boundaryField();
+
+    // Pressure
+    const volScalarField& p =
+        mesh_.lookupObject<volScalarField>("p");
+
+    // Density
+    // TODO Check
+    // TODO This probably only works with compressible solvers
+    const volScalarField& rho = mesh_.lookupObject<volScalarField>("rho");
+
+    int bufferIndex = 0;
+
+    // For every boundary patch of the interface
+    for (uint j = 0; j < patchIDs_.size(); j++)
+    {
+        int patchID = patchIDs_.at(j);
+
+        // Pressure forces
+        Force_->boundaryFieldRef()[patchID] =
+            Sfb[patchID] * p.boundaryField()[patchID] * rho;
+
+        // Viscous forces
+        Force_->boundaryFieldRef()[patchID] +=
+            Sfb[patchID] & devRhoReffb[patchID];
+
+        // Write the forces to the preCICE buffer
+        // For every cell of the patch
+        forAll(Force_->boundaryFieldRef()[patchID], i)
+        {
+            // Copy the force into the buffer
+            // x-dimension
+            buffer[bufferIndex++]
+            =
+            Force_->boundaryFieldRef()[patchID][i].x();
+
+            // y-dimension
+            buffer[bufferIndex++]
+            =
+            Force_->boundaryFieldRef()[patchID][i].y();
+
+            // z-dimension
+            buffer[bufferIndex++]
+            =
+            Force_->boundaryFieldRef()[patchID][i].z();
+        }
+    }
 }
 
 void preciceAdapter::FSI::Force::read(double * buffer)
@@ -51,6 +135,9 @@ void preciceAdapter::FSI::Force::read(double * buffer)
     * the outer for the locations and the inner for the dimensions.
     * See the preCICE readBlockVectorData() implementation.
     */
+    FatalErrorInFunction
+        << "Reading forces is not supported."
+        << exit(FatalError);
 }
 
 preciceAdapter::FSI::Force::~Force()
