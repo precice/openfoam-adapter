9d4ea6d435bb4a307581ea391b7c8583fad32674
Keefe Huang <huangq1234553@gmail.com>
2020-02-05 16:55:55 +0100

    Added adapter test file, AdapterTest.cc, custom include files AdapterUnittestIncludes.h and updated Adapter.H

--- Tests/UnitTests/AdapterTest/AdapterTest.cc
+++ Tests/UnitTests/AdapterTest/AdapterTest.cc
@@ -1,27 +1,74 @@
+#include "../MockFOAM/timeMock.h"
+#include "../MockFOAM/fvMeshMock.h"
+#include "../MockFOAM/fvCFDMock.h"
+#include "../MockFOAM/messageStreamMock.h"
+#include "../MockAdapter/CHTMock.h"
+#include "../MockAdapter/FSIMock.h"
+#include "../MockAdapter/InterfaceMock.h"
+
 #include "../../../Adapter.H"
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
-class AdapterTest : public ::testing::Test {
+using ::testing::Return;
+using ::testing::ReturnRef;
+
+class AdapterTest : public ::testing::Test
+{
  protected:
   void SetUp() override {
-      preciceAdapter::Adapter adapter_;
+//    adapter_= std::make_shared<preciceAdapter::Adapter>(runTime_, mesh_);
+      std::cout << "Creating new precice adapter\n";
+//    adapter_ = new preciceAdapter::Adapter(runTime_, mesh_);
+      adapter_ = new preciceAdapter::Adapter(runTime_, mesh_);
   }
-
+//
   void TearDown() override {
-      
+      delete adapter_;
   }
 
-private:
-    preciceAdapter::Adapter adapter;
+    // - OpenFOAM runTime object
+    Foam::Time runtime;
+    const Foam::Time& runTime_ = runtime;
 
+    //- OpenFOAM fvMesh object
+    Foam::fvMesh mesh;
+    const Foam::fvMesh& mesh_ = mesh;
+    preciceAdapter::Adapter* adapter_ = nullptr;
 };
 
 
-TEST(FactorialTest, Negative) {
+TEST_F(AdapterTest, SampleTest) {
 // This test is named "Negative", and belongs to the "FactorialTest"
 // test case.
-EXPECT_EQ(1, 1);
+    EXPECT_EQ(1, 1);
 }
+
+TEST_F(AdapterTest, ConfigureTest)
+{
+//    std::cout << "Out";
+    Foam::unwatchedIOdictionaryMock controlDict_;
+    std::string precice_config = "precice-config.xml";
+
+    EXPECT_CALL(runtime, processorCase()).WillOnce(Return(false));
+    EXPECT_CALL(controlDict_, lookupOrDefault("adjustTimeStep", false)).WillOnce(Return(true));
+    EXPECT_CALL(runtime, path()).WillOnce(Return("./"));
+    EXPECT_CALL(runtime, controlDict()).WillOnce(ReturnRef(controlDict_));
+    adapter_->configure();
+
+}
+//
+//TEST_F(AdapterTest, ExecuteTest){
+//    adapter_->execute();
+//}
+//
+//TEST_F(AdapterTest, AdjustTimeStepTest){
+//    adapter_->adjustTimeStep();
+//}
+//
+//TEST_F(AdapterTest, EndTest){
+//    adapter_->end();
+//}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
---
+++ Tests/UnitTests/AdapterTest/AdapterUnittestIncludes.h
@@ -0,0 +1,14 @@
+//
+// Created by keefe on 3/2/20.
+//
+
+#ifndef GTEST_ADAPTERUNITTESTINCLUDES_H
+#define GTEST_ADAPTERUNITTESTINCLUDES_H
+
+#include "Tests/UnitTests/MockAdapter/InterfaceMock.h"
+#include "Tests/UnitTests/MockAdapter/CHTMock.h"
+#include "Tests/UnitTests/MockAdapter/FSIMock.h"
+#include "Tests/UnitTests/MockFOAM/timeMock.h"
+#include "Tests/UnitTests/MockFOAM/fvMeshMock.h"
+#include "Tests/UnitTests/MockpreCICE/SolverInterfaceMock.hpp"
+#endif //GTEST_ADAPTERUNITTESTINCLUDES_H
