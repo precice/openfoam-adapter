b4e0683c0348e5b93b080f8a93a9062fe82036d1
Keefe Huang <huangq1234553@gmail.com>
2020-02-07 22:53:18 +0100

    Update includes. Remove unneeded includes in AdapterUnittestIncludes.h

--- Tests/UnitTests/AdapterTest/AdapterUnittestIncludes.h
+++ Tests/UnitTests/AdapterTest/AdapterUnittestIncludes.h
@@ -5,10 +5,15 @@
 #ifndef GTEST_ADAPTERUNITTESTINCLUDES_H
 #define GTEST_ADAPTERUNITTESTINCLUDES_H
 
+#include "Tests/UnitTests/MockAdapter/UtilitiesMock.h"
 #include "Tests/UnitTests/MockAdapter/InterfaceMock.h"
 #include "Tests/UnitTests/MockAdapter/CHTMock.h"
 #include "Tests/UnitTests/MockAdapter/FSIMock.h"
+
 #include "Tests/UnitTests/MockFOAM/timeMock.h"
 #include "Tests/UnitTests/MockFOAM/fvMeshMock.h"
+#include "Tests/UnitTests/MockFOAM/messageStreamMock.h"
+
 #include "Tests/UnitTests/MockpreCICE/SolverInterfaceMock.hpp"
+
 #endif //GTEST_ADAPTERUNITTESTINCLUDES_H
