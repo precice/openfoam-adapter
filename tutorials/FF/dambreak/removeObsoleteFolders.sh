895add1f1174c2084c66572d2d4f1e8f70105382
Francisco Espinosa <paco.espinosa.pelaez@gmail.com>
2020-08-04 12:45:26 +0200

    preparing interFoam_solver

--- tutorials/FF/dambreak/removeObsoleteFolders.sh
+++ tutorials/FF/dambreak/removeObsoleteFolders.sh
@@ -2,7 +2,7 @@
 
 echo "Looking for any time directories without results (e.g. stray functionObjectProperties files, see issue #26 on GitHub)..."
 
-cd Fluid1
+cd interFoam_solver
 for f in [0-9]* [0-9]*.[0-9]*; do
 	if ! [ -f $f/U ] && ! [ -f $f/T ]; then
 		rm -rfv $f
@@ -20,23 +20,3 @@ if [ -d processor0 ]; then
 	done
 fi
 cd ..
-
-cd Fluid2
-for f in [0-9]* [0-9]*.[0-9]*; do
-	if ! [ -f $f/U ] && ! [ -f $f/T ]; then
-		rm -rfv $f
-	fi
-done
-if [ -d processor0 ]; then
-	for g in processor*; do
-		cd $g
-		for f in [0-9]* [0-9]*.[0-9]*; do
-			if ! [ -f $f/U ] && ! [ -f $f/T ]; then
-				rm -rfv $f
-			fi
-		done
-		cd ..
-	done
-fi
-cd ..
-
