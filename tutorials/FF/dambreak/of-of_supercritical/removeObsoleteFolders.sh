71e87b98f9de23fe54e6f3742b59911131ff84c6
Francisco Espinosa <paco.espinosa.pelaez@gmail.com>
2020-08-31 03:59:52 +0200

    Add of_monolothic_supercritical and of-of_supercritical

---
+++ tutorials/FF/dambreak/of-of_supercritical/removeObsoleteFolders.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+echo "Looking for any time directories without results (e.g. stray functionObjectProperties files, see issue #26 on GitHub)..."
+
+cd IF_Left
+for f in [0-9]* [0-9]*.[0-9]*; do
+	if ! [ -f $f/U ] && ! [ -f $f/T ]; then
+		rm -rfv $f
+	fi
+done
+if [ -d processor0 ]; then
+	for g in processor*; do
+		cd $g
+		for f in [0-9]* [0-9]*.[0-9]*; do
+			if ! [ -f $f/U ] && ! [ -f $f/T ]; then
+				rm -rfv $f
+			fi
+		done
+		cd ..
+	done
+fi
+cd ..
+
+cd IF_Right
+for f in [0-9]* [0-9]*.[0-9]*; do
+	if ! [ -f $f/U ] && ! [ -f $f/T ]; then
+		rm -rfv $f
+	fi
+done
+if [ -d processor0 ]; then
+	for g in processor*; do
+		cd $g
+		for f in [0-9]* [0-9]*.[0-9]*; do
+			if ! [ -f $f/U ] && ! [ -f $f/T ]; then
+				rm -rfv $f
+			fi
+		done
+		cd ..
+	done
+fi
+cd ..
+
