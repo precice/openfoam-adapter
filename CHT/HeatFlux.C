f0c120aa8fea7e704b1bce836aac14abc79f090f
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2021-05-06 15:50:14 +0200

    OpenFOAM8-specific changes
    Disable execute at start for OpenFOAM8 (#180)
    Adapt KappaEffective to new transport/thermophysical model structure (OpenFOAM 8) (#130)
    
    Co-authored-by: Gerasimos Chourdakis <chourdak@in.tum.de>
    Co-authored-by: David Schneider <david.schneider@ipvs.uni-stuttgart.de>

--- CHT/HeatFlux.C
+++ CHT/HeatFlux.C
@@ -160,7 +160,7 @@ preciceAdapter::CHT::HeatFlux_Incompressible::HeatFlux_Incompressible(
     const std::string namePr,
     const std::string nameAlphat)
 : HeatFlux(mesh, nameT),
-  Kappa_(new KappaEff_Incompressible(mesh, nameRho, nameCp, namePr, nameAlphat))
+  Kappa_(new KappaEff_Incompressible(mesh))
 {
 }
 
