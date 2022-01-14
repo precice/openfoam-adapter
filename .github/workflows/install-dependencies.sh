f34dfd259542f01b623256b31d86819a0f720d0e
Gerasimos Chourdakis <makishourdakis@gmail.com>
2022-01-14 08:32:54 +0100

    Bump versions to OpenFOAM v2112 and preCICE v2.3.0 (#211)
    * Bump versions to OpenFOAM v2112 and preCICE v2.3.0
    * Switch clang-format-action version to main

--- .github/workflows/install-dependencies.sh
+++ .github/workflows/install-dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-# Install OpenFOAM v2106
+# Install OpenFOAM v2112
 wget -q -O - https://dl.openfoam.com/add-debian-repo.sh | sudo bash
-sudo apt-get install openfoam2106-dev
+sudo apt-get install openfoam2112-dev
 
-# Install preCICE v2.2.1
-wget https://github.com/precice/precice/releases/download/v2.2.1/libprecice2_2.2.1_focal.deb
-sudo apt install ./libprecice2_2.2.1_focal.deb
+# Install preCICE v2.3.0
+wget https://github.com/precice/precice/releases/download/v2.3.0/libprecice2_2.3.0_focal.deb
+sudo apt install ./libprecice2_2.3.0_focal.deb
