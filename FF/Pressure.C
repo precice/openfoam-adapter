ee2d09589548b20e021ee415572fc0b091ca506a
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2023-05-26 23:34:21 +0200

    Remove unnecessary initialization fix

--- FF/Pressure.C
+++ FF/Pressure.C
@@ -35,11 +35,6 @@ void preciceAdapter::FF::Pressure::write(double* buffer, bool meshConnectivity, 
 void preciceAdapter::FF::Pressure::read(double* buffer, const unsigned int dim)
 {
     int bufferIndex = 0;
-    if (firstRead)
-    {
-        firstRead = false;
-        return;
-    }
 
     // For every boundary patch of the interface
     for (uint j = 0; j < patchIDs_.size(); j++)
