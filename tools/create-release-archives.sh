acfc3e31a7eba37b2f920167a61897c3bb620f4f
Gerasimos Chourdakis <chourdak@in.tum.de>
2023-06-20 17:28:21 +0200

    Update tools/create-release-archives.sh
    Co-authored-by: David Schneider <david.schneider@ipvs.uni-stuttgart.de>

--- tools/create-release-archives.sh
+++ tools/create-release-archives.sh
@@ -8,6 +8,7 @@ adapter_version="v1.2.3"
 # Declare branch names for which archives will be created
 declare -a branches=("master" "OpenFOAMv1806" "OpenFOAM10" "OpenFOAM9" "OpenFOAM8" "OpenFOAM7" "OpenFOAM6" "OpenFOAM5" "OpenFOAM4")
 
+mkdir -p release-archives
 for i in "${branches[@]}"
     do
     archive_name=openefoam-adapter-"${adapter_version}"-"${i}"
