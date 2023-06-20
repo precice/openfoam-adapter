7172e87d53ec59da5c464b63f14b1188da476c9a
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2023-06-20 15:27:52 +0200

    Rename variable

--- tools/create-release-archives.sh
+++ tools/create-release-archives.sh
@@ -6,9 +6,9 @@ remote_name=upstream
 adapter_version="v1.2.3"
 
 # Declare branch names for which archives will be created
-declare -a tests=("master" "OpenFOAMv1806" "OpenFOAM10" "OpenFOAM9" "OpenFOAM8" "OpenFOAM7" "OpenFOAM6" "OpenFOAM5" "OpenFOAM4")
+declare -a branches=("master" "OpenFOAMv1806" "OpenFOAM10" "OpenFOAM9" "OpenFOAM8" "OpenFOAM7" "OpenFOAM6" "OpenFOAM5" "OpenFOAM4")
 
-for i in "${tests[@]}"
+for i in "${branches[@]}"
     do
     archive_name=openefoam-adapter-"${adapter_version}"-"${i}"
     git archive --format=tar.gz --prefix="${archive_name}"/ "${remote_name}"/"${i}" >"${archive_name}".tar.gz
