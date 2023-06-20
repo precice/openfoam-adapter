1608737ab2682695b2b6e3d38f9c541b6ea70603
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2023-06-20 15:26:14 +0200

    Add simple script to create all the release archives

---
+++ tools/create-release-archives.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -u
+
+remote_name=upstream
+adapter_version="v1.2.3"
+
+# Declare branch names for which archives will be created
+declare -a tests=("master" "OpenFOAMv1806" "OpenFOAM10" "OpenFOAM9" "OpenFOAM8" "OpenFOAM7" "OpenFOAM6" "OpenFOAM5" "OpenFOAM4")
+
+for i in "${tests[@]}"
+    do
+    archive_name=openefoam-adapter-"${adapter_version}"-"${i}"
+    git archive --format=tar.gz --prefix="${archive_name}"/ "${remote_name}"/"${i}" >"${archive_name}".tar.gz
+done
