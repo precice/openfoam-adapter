01caf716871b8d9f824a0a5678dd519159ada8cd
Gerasimos Chourdakis <chourdak@in.tum.de>
2023-07-19 22:40:43 +0200

    Rename focal to jammy for preCICE packages

--- .github/workflows/install-dependencies.sh
+++ .github/workflows/install-dependencies.sh
@@ -5,5 +5,5 @@ wget -q -O - https://dl.openfoam.com/add-debian-repo.sh | sudo bash
 sudo apt-get install openfoam2306-dev
 
 # Install preCICE v2.5.0
-wget https://github.com/precice/precice/releases/download/v2.5.0/libprecice2_2.5.0_focal.deb
-sudo apt install ./libprecice2_2.5.0_focal.deb
+wget https://github.com/precice/precice/releases/download/v2.5.0/libprecice2_2.5.0_jammy.deb
+sudo apt install ./libprecice2_2.5.0_jammy.deb
