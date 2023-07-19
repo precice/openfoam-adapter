7ee5219d3c542b58ae8a46c48f870773e0aa7cdd
Gerasimos Chourdakis <chourdak@in.tum.de>
2023-07-19 23:08:31 +0200

    Switch default CI build back to Ubuntu 20.04

--- .github/workflows/install-dependencies.sh
+++ .github/workflows/install-dependencies.sh
@@ -2,7 +2,7 @@
 
 # Install OpenFOAM v2306
 wget -q -O - https://dl.openfoam.com/add-debian-repo.sh | sudo bash
-sudo apt-get install openfoam2212-dev
+sudo apt-get install openfoam2306-dev
 
 # Install preCICE v2.5.0
 wget https://github.com/precice/precice/releases/download/v2.5.0/libprecice2_2.5.0_jammy.deb
