a2362f3435ed31d08454871d0b50f422e320b82b
Gerasimos Chourdakis <makishourdakis@gmail.com>
2023-07-24 22:14:44 +0300

    Bump default OpenFOAM version to v2306 (#296)

--- .github/workflows/install-dependencies.sh
+++ .github/workflows/install-dependencies.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
-# Install OpenFOAM v2212
+# Install OpenFOAM v2306
 wget -q -O - https://dl.openfoam.com/add-debian-repo.sh | sudo bash
-sudo apt-get install openfoam2212-dev
+sudo apt-get update
+sudo apt-get install openfoam2306-dev
 
 # Install preCICE v2.5.0
-wget https://github.com/precice/precice/releases/download/v2.5.0/libprecice2_2.5.0_focal.deb
-sudo apt install ./libprecice2_2.5.0_focal.deb
+wget https://github.com/precice/precice/releases/download/v2.5.0/libprecice2_2.5.0_jammy.deb
+sudo apt install ./libprecice2_2.5.0_jammy.deb
