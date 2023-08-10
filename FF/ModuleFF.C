766f4c2b9afacdb8cb5bc57251435bf97fe60b84
Gerasimos Chourdakis <chourdak@in.tum.de>
2023-08-10 12:47:45 +0200

    Group build targets per module

---
+++ FF/ModuleFF.C
@@ -0,0 +1,6 @@
+// Central inlude file to reduce the number of build targets and build time
+#include "FF.C"
+#include "Velocity.C"
+#include "VelocityGradient.C"
+#include "Pressure.C"
+#include "PressureGradient.C"
