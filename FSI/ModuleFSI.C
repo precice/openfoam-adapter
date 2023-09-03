61e7f6913b4cd980e9e244f08f83407536675a51
Gerasimos Chourdakis <makishourdakis@gmail.com>
2023-09-03 19:04:27 +0300

    Reduce build targets to save building time (#301)

---
+++ FSI/ModuleFSI.C
@@ -0,0 +1,7 @@
+// Central inlude file to reduce the number of build targets and build time
+#include "FSI.C"
+#include "ForceBase.C"
+#include "Force.C"
+#include "Stress.C"
+#include "Displacement.C"
+#include "DisplacementDelta.C"
