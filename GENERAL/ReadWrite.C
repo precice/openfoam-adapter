b456dc962452ddedf7bddd8435c4af1665dfb16d
Daniels <d.vidulejs@tum.de>
2025-04-14 20:41:29 +0200

    Create VectorFieldCoupler for volume coupling. missing surface coupling.

--- GENERAL/ReadWrite.C
+++ GENERAL/ReadWrite.C
@@ -21,6 +21,8 @@ preciceAdapter::MODULE::ScalarFieldCoupler::ScalarFieldCoupler(
     dataType_ = scalar;
 }
 
+// only surface field coupling ATM (boundaries)
+
 std::size_t preciceAdapter::MODULE::ScalarFieldCoupler::write(double* buffer, bool meshConnectivity, const unsigned int dim)
 {
     // TODO meshconnectivity
@@ -107,22 +109,155 @@ void preciceAdapter::MODULE::ScalarFieldCoupler::read(double* buffer, const unsi
 
 bool preciceAdapter::MODULE::ScalarFieldCoupler::isLocationTypeSupported(const bool meshConnectivity) const
 {
-    // For cases with mesh connectivity, we support:
-    // - face nodes, only for writing
-    // - face centers, only for reading
-    // However, since we do not distinguish between reading and writing in the code, we
-    // always return true and offload the handling to the user.
-    if (meshConnectivity)
-    {
-        return (this->locationType_ == LocationType::faceCenters || this->locationType_ == LocationType::faceNodes); // we currently do not support meshConnectivity for volumeCenters
-    }
-    else
-    {
-        return (this->locationType_ == LocationType::faceCenters);
-    }
+    // TODO
+    return false;
 }
 
 std::string preciceAdapter::MODULE::ScalarFieldCoupler::getDataName() const
+{
+    return fieldConfig_.name;
+}
+
+//----- preciceAdapter::MODULE::VectorFieldCoupler -----------------------------------------
+
+preciceAdapter::MODULE::VectorFieldCoupler::VectorFieldCoupler(
+    const Foam::fvMesh& mesh,
+    const struct fieldConfig& fieldConfig)
+: vectorField_(
+    const_cast<volVectorField*>(
+        &mesh.lookupObject<volVectorField>(fieldConfig.solver_name))),
+  mesh_(mesh),
+  fieldConfig_(fieldConfig)
+{
+    dataType_ = vector;
+}
+
+std::size_t preciceAdapter::MODULE::VectorFieldCoupler::write(double* buffer, bool meshConnectivity, const unsigned int dim)
+{
+    int bufferIndex = 0;
+
+    if (this->locationType_ == LocationType::volumeCenters)
+    {
+        if (cellSetNames_.empty())
+        {
+            for (const auto& cell : vectorField_->internalField())
+            {
+                // x-dimension
+                buffer[bufferIndex++] = cell.x();
+
+                // y-dimension
+                buffer[bufferIndex++] = cell.y();
+
+                if (dim == 3)
+                {
+                    // z-dimension
+                    buffer[bufferIndex++] = cell.z();
+                }
+            }
+        }
+        else
+        {
+            for (const auto& cellSetName : cellSetNames_)
+            {
+                cellSet overlapRegion(vectorField_->mesh(), cellSetName);
+                const labelList& cells = overlapRegion.toc();
+
+                for (const auto& currentCell : cells)
+                {
+                    // x-dimension
+                    buffer[bufferIndex++] = vectorField_->internalField()[currentCell].x();
+
+                    // y-dimension
+                    buffer[bufferIndex++] = vectorField_->internalField()[currentCell].y();
+
+                    if (dim == 3)
+                    {
+                        // z-dimension
+                        buffer[bufferIndex++] = vectorField_->internalField()[currentCell].z();
+                    }
+                }
+            }
+        }
+    }
+
+    // For every boundary patch of the interface
+    for (uint j = 0; j < patchIDs_.size(); j++)
+    {
+        int patchID = patchIDs_.at(j);
+
+        // Get the velocity value boundary patch
+
+        // For every cell of the patch
+    }
+    return bufferIndex;
+}
+
+void preciceAdapter::MODULE::VectorFieldCoupler::read(double* buffer, const unsigned int dim)
+{
+    int bufferIndex = 0;
+
+    if (this->locationType_ == LocationType::volumeCenters)
+    {
+        if (cellSetNames_.empty())
+        {
+            for (auto& cell : vectorField_->ref())
+            {
+                // x-dimension
+                cell.x() = buffer[bufferIndex++];
+
+                // y-dimension
+                cell.y() = buffer[bufferIndex++];
+
+                if (dim == 3)
+                {
+                    // z-dimension
+                    cell.z() = buffer[bufferIndex++];
+                }
+            }
+        }
+        else
+        {
+            for (const auto& cellSetName : cellSetNames_)
+            {
+                cellSet overlapRegion(vectorField_->mesh(), cellSetName);
+                const labelList& cells = overlapRegion.toc();
+
+                for (const auto& currentCell : cells)
+                {
+                    // x-dimension
+                    vectorField_->ref()[currentCell].x() = buffer[bufferIndex++];
+
+                    // y-dimension
+                    vectorField_->ref()[currentCell].y() = buffer[bufferIndex++];
+
+                    if (dim == 3)
+                    {
+                        // z-dimension
+                        vectorField_ > ref()[currentCell].z() = buffer[bufferIndex++];
+                    }
+                }
+            }
+        }
+    }
+
+    // For every boundary patch of the interface
+    for (uint j = 0; j < patchIDs_.size(); j++)
+    {
+        int patchID = patchIDs_.at(j);
+
+        // Get the velocity value boundary patch
+
+        // For every cell of the patch
+    }
+}
+
+bool preciceAdapter::MODULE::VectorFieldCoupler::isLocationTypeSupported(const bool meshConnectivity) const
+{
+    // TODO
+    return false;
+}
+
+std::string preciceAdapter::MODULE::VectorFieldCoupler::getDataName() const
 {
     return fieldConfig_.name;
 }
