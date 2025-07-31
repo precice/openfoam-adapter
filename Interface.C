a4dc6367ef2d58b9f95f9283a47433e5bdc3d123
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2025-07-31 23:04:49 +0200

    Merge initialization and set of nReadData

--- Interface.C
+++ Interface.C
@@ -554,8 +554,7 @@ try
 
         // Make preCICE read vector or scalar data
         // and fill the adapter's buffer
-        std::size_t nReadData = 0;
-        nReadData = vertexIDs_.size() * precice_.getDataDimensions(meshName_, couplingDataReader->dataName());
+        std::size_t nReadData = vertexIDs_.size() * precice_.getDataDimensions(meshName_, couplingDataReader->dataName());
 
         // We could add a sanity check here
         // nReadData == vertexIDs_.size() * (1 + (dim_ - 1) * static_cast<int>(couplingDataReader->hasVectorData()));
