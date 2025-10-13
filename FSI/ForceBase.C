063ed732cee5157fb9d3145c4588cdc475103818
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2025-10-13 11:17:21 +0200

    Clarify error message when reading stresses

--- FSI/ForceBase.C
+++ FSI/ForceBase.C
@@ -193,12 +193,14 @@ std::size_t preciceAdapter::FSI::ForceBase::writeToBuffer(double* buffer,
 
 void preciceAdapter::FSI::ForceBase::readFromBuffer(double* buffer) const
 {
-    /* TODO: Implement
+    /* TODO: Implement (issue https://github.com/precice/openfoam-adapter/issues/240)
      * We need two nested for-loops for each patch,
      * the outer for the locations and the inner for the dimensions.
-     * See the preCICE readBlockVectorData() implementation.
+     * Since the Force class already implements a read method,
+     * this only corresponds to stresses.
+     * In general, consider refactoring the ForceBase, Force, and Stress classes.
      */
     FatalErrorInFunction
-        << "Reading forces is not supported."
+        << "Reading stresses is not yet supported."
         << exit(FatalError);
 }
